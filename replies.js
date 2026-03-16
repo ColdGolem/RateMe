c68104c77b90807977d06a2a973773af43611adc
ColdGolem <deeperseekerz@gmail.com>
2026-03-17 03:26:43 +0530

    Update replies.js

--- replies.js
+++ replies.js
@@ -1,1 +1,12 @@
 
+const replies = [
+
+"You look like someone who starts plans and disappears halfway.",
+"Suspiciously confident for someone who probably forgot why they opened the fridge.",
+"Chaotic energy with unexpected charm.",
+"You look like the friend who says 'one more episode' and watches five.",
+"Calm on the outside, mild disaster internally.",
+"Somehow both responsible and the reason problems exist.",
+"You give strong 'I know what I'm doing' energy... even if you don't."
+
+];
